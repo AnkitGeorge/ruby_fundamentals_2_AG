7e19e2759b9c1fc64b86376512d858cbaa746110
AnkitGeorge <ankit.george@gmail.com>
2016-01-14 05:50:41 -0500

    Complete exercise 6.

--- exercise6.rb
+++ exercise6.rb
@@ -7,7 +7,7 @@ grocery_list.each { |item| puts "*#{item}"}
 # each iterator to print items in the list
 # Forgot rice. Will add to array
 
-puts "\n\n"
+puts "\n"
 
 grocery_list << "rice"
 
@@ -16,12 +16,26 @@ def print_list(list_raw)
 end
 # Define method print_list to print items on every line with an asterisk.
 
+puts "Added rice."
 print_list(grocery_list)
 
 puts "#{grocery_list.length} items on your list."
 
 if grocery_list.include?("bananas") == true
-  puts "\nYou do not need to pick up bananas."
+  puts "\nYou do not need to pick up bananas.\n\n"
 else
-  puts "\nYou need to pick up bananas."
+  puts "\nYou need to pick up bananas.\n\n"
 end
+
+puts "#{grocery_list[1]}"
+
+grocery_list.sort
+
+puts "\nYour alphabetized list is as follows:"
+print_list(grocery_list)
+
+grocery_list.delete("salmon")
+# Remove salmon as it is not to be found.
+
+puts "\nYour final list is as follows, as salmon cannot be found:"
+print_list(grocery_list)
